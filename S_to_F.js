3d6c01cb252e7356967a507eb5d0528a241218e3
Michael Espey <espemi01@luther.edu>
2014-10-29 21:30:00 -0500

    Adding more js files

--- S_to_F.js
+++ S_to_F.js
@@ -1,24 +1,2 @@
-<script>
-    
-//Spotify
-Client ID: 21110200b1da4bf7a591bc91b16ef0f0
-Client Secret: 935c9ccb7de24f9a82a89abc945af306
-
-//Facebook
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '577702369027634',
-      xfbml      : true,
-      version    : 'v2.1'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-    
-</script>
+import Spotify.js
+import Facebook.js
