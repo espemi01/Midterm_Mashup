a862ac4ae366030527f55b87ed2fd8aea4678049
Michael Espey <espemi01@luther.edu>
2014-10-31 12:48:10 -0500

    typo

--- s.js
+++ s.js
@@ -2,7 +2,7 @@
 //Client ID: 21110200b1da4bf7a591bc91b16ef0f0
 //Client Secret: 935c9ccb7de24f9a82a89abc945af306
 
-createWidget = funcion() {
+createWidget = function() {
     widgetList = document.querySelector("#widgetlist");
     widget = document.createElement("IFRAME");
     var name = artistName();
