e8589c514a586c2eb4b68ff258f2d62f89b0b492
Michael Espey <espemi01@luther.edu>
2014-11-03 11:31:08 -0600

    ao;gh

--- s.js
+++ s.js
@@ -48,7 +48,7 @@ getArtistID = function(name) {
     request.open('GET', theResource , true)
     request.setRequestHeader('Access-Control-Allow-Origin','http://espemi01.github.io')
     request.setRequestHeader('Access-Control-Request-Method','POST')
-    
+    request.setRequestHeader('Access-Control-Request-Headers','X-Custom-Header')
     
     request.send(null)	
 }
