7be683a8ffa756e513ee01b9f1c945181c1df952
arenstlouis <stloar01@luther.edu>
2014-11-03 13:55:27 -0600

    Change

--- s.js
+++ s.js
@@ -44,7 +44,7 @@ getArtistID = function(name) {
     console.log(name)
     
     request.open('GET', theResource , true)
-    request.setRequestHeader('Access-Control-Allow-Origin','http://espemi01.github.io')
+    request.setRequestHeader('Origin: http://espemi01.github.io', 'Access-Control-Allow-Origin: http://espemi01.github.io')
     
     request.send(null)	
 }
