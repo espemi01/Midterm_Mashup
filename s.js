06aa7de5b4d4aa5b8dca666f45bf6049ce4765d9
Michael Espey <espemi01@luther.edu>
2014-11-03 13:24:33 -0600

    fb cred

--- s.js
+++ s.js
@@ -27,7 +27,6 @@ getArtistID = function(name) {
     var theResource = 'https://ws.spotify.com/search/1/artist.json?q='+name
 
     request.onload = function() {
-        //if (request.readyState == 4 && request.status == 200) {
             console.log(request.responseText)
 
             var res = eval( '(' + request.responseText + ')' )
