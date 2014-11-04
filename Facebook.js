4a7b1026b55acee1d1dc131bf67bedda08cba667
Michael Espey <espemi01@luther.edu>
2014-11-03 21:14:14 -0600

    aoeihf

--- Facebook.js
+++ Facebook.js
@@ -109,7 +109,7 @@ function giveMusic() {
                     for (i = 0; i < MR.data.length; i++) {
                         if (MR.data[i].category == 'Musician/band') {
                             aList.push(MR.data[i].name)
-                            //console.log(MR.data[i].name)
+                            console.log(MR.data[i].name)
                         }
                     }
                 });
@@ -117,8 +117,8 @@ function giveMusic() {
         });
     });
     //document.getElementById('aList').innerHTML =  aList;
-    console.log(aList.length)
-    takeMusic(aList)
+    //console.log(aList.length)
+    //takeMusic(aList)
     //FB.logout()
 }
 
