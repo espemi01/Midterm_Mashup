1bb04d19d6cde0211a0d2ce973660612cd6b65ea
Michael Espey <espemi01@luther.edu>
2014-11-04 00:19:07 -0600

    osdhsd

--- s.js
+++ s.js
@@ -6,22 +6,22 @@ CW = function(iName) {
     widgetList = document.querySelector("#widgetlist");
     widget = document.createElement("IFRAME");
     var name = artistName(iName);
-    getArtistID(name);
+    makeW(name);
 }
-callback = function(artistID) {
-    widget.setAttribute("src","https://embed.spotify.com/follow/1/?uri="+artistID+"&size=detail&theme=dark");
-    widget.style.width = 300;
-    widget.style.height = 56;
-    widget.style.frameborder = none;
-    widgetlist.appendChild(widget);
-}
+//callback = function(artistID) {
+//    widget.setAttribute("src","https://embed.spotify.com/follow/1/?uri="+artistID+"&size=detail&theme=dark");
+//    widget.style.width = 300;
+//    widget.style.height = 56;
+//    widget.style.frameborder = none;
+//    widgetlist.appendChild(widget);
+//}
 artistName = function(iName) {
     //name = document.getElementById("artistIN").value;
     name = iName.split(' ').join('_')
     console.log(name)
     return name
 }
-getArtistID = function(name) {
+makeW = function(name) {
     var request = new XMLHttpRequest()
     var theResource = 'https://ws.spotify.com/search/1/artist.json?q='+name
 
