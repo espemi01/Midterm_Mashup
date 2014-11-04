36e0a09b87c302ee1d7c9cda717e19aba12c1946
Michael Espey <espemi01@luther.edu>
2014-11-03 21:10:41 -0600

    oadih

--- S_to_F.js
+++ S_to_F.js
@@ -1,6 +1,6 @@
-function takeMusic(tList) {
-    for (i = 0; i < tList.length(); i++) {
-        console.log(tList[i]);
-        console.log(i)
-    }
-}
+//function takeMusic(tList) {
+//    for (i = 0; i < tList.length(); i++) {
+//        console.log(tList[i]);
+//        console.log(i)
+//    }
+//}
