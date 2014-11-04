5444fa9414ecb0d764b4328f1e255cd9f5eb210a
Michael Espey <espemi01@luther.edu>
2014-11-03 22:42:08 -0600

    osdifh

--- Facebook.js
+++ Facebook.js
@@ -124,11 +124,3 @@ function giveMusic() {
         });
     });
 }
-
-//function FBcb(aList) {
-//    var aList = aList;
-//    for (var a = 0; a < aList.length; a++) {
-//        console.log(aList[a]);
-//        console.log(a);
-//    };
-//}
